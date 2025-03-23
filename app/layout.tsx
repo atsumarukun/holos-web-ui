a0d0b2af9b05c31df02014b055e5dabd57332ba5
atsumarukun <atys.smz@gmail.com>
2025-03-23 23:54:11 +0900

    update: htmlのlangをjaに変更.

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
         className={`h-screen ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
