354d3742550031199ebf1f37be411b698f39871b
atsumarukun <atys.smz@gmail.com>
2025-03-28 00:30:51 +0900

    fix: リクエストエンドポイントを修正.

--- features/auth/actions/signup.ts
+++ features/auth/actions/signup.ts
@@ -6,14 +6,17 @@ import { Account, accountSchema } from "../schemas/account";
 import { Signup } from "../schemas/signup";
 
 export const signup = async (data: Signup): Promise<Account> => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_AUTH_SERVER_HOST}/users`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(toSnakeCase(data)),
-    cache: "no-cache",
-  });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_ACCOUNT_API_HOST}/accounts`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(toSnakeCase(data)),
+      cache: "no-cache",
+    }
+  );
 
   if (!res.ok) {
     const err = errorSchema.parse(await res.json());
