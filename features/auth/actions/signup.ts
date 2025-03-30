8a8f51ab9009b54e2e37d0149d22a125bfdc9b92
atsumarukun <atys.smz@gmail.com>
2025-03-30 19:42:54 +0900

    update: signupのエラーハンドリングを変更.

--- features/auth/actions/signup.ts
+++ features/auth/actions/signup.ts
@@ -1,27 +1,61 @@
 "use server";
 
+import {
+  ActionsError,
+  actionsErrorCode,
+  toActionsErrorCode,
+} from "@/lib/actions-error";
 import { toCamelCase, toSnakeCase } from "@/lib/case-converters";
 import { errorSchema } from "@/schemas/error";
 import { Account, accountSchema } from "../schemas/account";
 import { Signup } from "../schemas/signup";
 
-export const signup = async (data: Signup): Promise<Account> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_ACCOUNT_API_HOST}/accounts`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(toSnakeCase(data)),
-      cache: "no-cache",
+export const signup = async (data: Signup): Promise<Account | ActionsError> => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_ACCOUNT_API_HOST}/accounts`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(toSnakeCase(data)),
+        cache: "no-cache",
+      }
+    );
+
+    if (!res.ok) {
+      const result = errorSchema.safeParse(await res.json());
+      if (result.success) {
+        return {
+          type: "ActionsError",
+          message: result.data.message,
+          code: toActionsErrorCode(res.status),
+        };
+      } else {
+        throw new Error("invalid fetch response");
+      }
     }
-  );
 
-  if (!res.ok) {
-    const err = errorSchema.parse(await res.json());
-    throw new Error(err.message, { cause: res.status });
+    const result = accountSchema.safeParse(toCamelCase(await res.json()));
+    if (result.success) {
+      return result.data;
+    } else {
+      throw new Error("invalid fetch response");
+    }
+  } catch (e) {
+    if (e instanceof Error) {
+      return {
+        type: "ActionsError",
+        message: e.message,
+        code: actionsErrorCode.Unknown,
+      };
+    } else {
+      return {
+        type: "ActionsError",
+        message: "failed to signup",
+        code: actionsErrorCode.Unknown,
+      };
+    }
   }
-
-  return accountSchema.parse(toCamelCase(await res.json()));
 };
--- features/auth/hooks/signup.tsx
+++ features/auth/hooks/signup.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ActionsError, isActionsError } from "@/lib/actions-error";
 import { useState } from "react";
 import { SubmitHandler } from "react-hook-form";
 import { signup } from "../actions/signup";
@@ -8,29 +9,23 @@ import { Signup } from "../schemas/signup";
 
 type Props = Readonly<{
   onCompleted?: (data?: Account) => void;
-  onError?: (err?: Error) => void;
+  onError?: (err?: ActionsError) => void;
 }>;
 
 export const useSignup = (props?: Props) => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<Account | undefined>(undefined);
-  const [error, setError] = useState<Error | undefined>(undefined);
+  const [error, setError] = useState<ActionsError | undefined>(undefined);
 
   const onSubmit: SubmitHandler<Signup> = async (data) => {
     setLoading(true);
-    try {
-      const account = await signup(data);
-      setData(account);
-      props?.onCompleted?.(account);
-    } catch (e) {
-      if (e instanceof Error) {
-        setError(e);
-        props?.onError?.(e);
-      } else {
-        const err = new Error("failed to sign up");
-        setError(err);
-        props?.onError?.(err);
-      }
+    const res = await signup(data);
+    if (isActionsError(res)) {
+      setError(res);
+      props?.onError?.(res);
+    } else {
+      setData(res);
+      props?.onCompleted?.(res);
     }
     setLoading(false);
   };
