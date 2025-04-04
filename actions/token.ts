6e425f5da09f44221616e01e55f0cec1c635d056
atsumarukun <atys.smz@gmail.com>
2025-04-04 19:48:04 +0900

    create: setTokenアクションを作成.

--- actions/token.ts
+++ actions/token.ts
@@ -2,6 +2,15 @@
 
 import { cookies } from "next/headers";
 
+export const setToken = async (token: string) => {
+  const store = await cookies();
+  store.set("token", token, {
+    httpOnly: true,
+    path: "/",
+    maxAge: 60 * 60 * 24 * 7, // 7日
+  });
+};
+
 export const getToken = async (): Promise<string | undefined> => {
   const store = await cookies();
   return store.get("token")?.value;
