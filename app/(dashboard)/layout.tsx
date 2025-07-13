62e69225eb1389284df1a92b41ac57b394dfacbc
atsumarukun <atys.smz@gmail.com>
2025-07-14 00:43:37 +0900

    update: LayoutにSPデザインを追加.

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,8 @@
+import { Header } from "@/components/organisms/Header";
 import { Nav } from "@/components/organisms/Nav";
+import { buildClassName } from "@/lib/class-name";
 import { headers } from "next/headers";
+import Link from "next/link";
 
 export default async function DashboardLayout({
   children,
@@ -10,18 +13,31 @@ export default async function DashboardLayout({
   const accountName = header.get("account-name") ?? "";
 
   return (
-    <div className="h-full flex flex-row">
-      <div className="h-full flex flex-col justify-between">
-        <Nav />
-        <div className="">
-          <button className="w-full text-lg text-left font-normal border-t border-separator px-6 py-4">
-            {accountName}
-          </button>
+    <>
+      <div className="block md:hidden">
+        <Header accountName={accountName} />
+      </div>
+      <div className="h-full flex flex-row">
+        <div className="h-full md:flex flex-col justify-between hidden">
+          <Link
+            href="/"
+            className={buildClassName(
+              "w-full inline-block font-playwrite text-xl px-6 py-4 mt-2 mb-6"
+            )}
+          >
+            H<span className="text-theme">o</span>los
+          </Link>
+          <Nav className="w-72" />
+          <div className="border-t border-separator">
+            <button className="w-full text-lg text-left font-normal px-6 py-4">
+              {accountName}
+            </button>
+          </div>
+        </div>
+        <div className="grow md:bg-[rgba(0,0,0,0.05)] md:dark:bg-[rgba(255,255,255,0.05)] pt-14 md:pt-0">
+          {children}
         </div>
       </div>
-      <div className="grow bg-[rgba(0,0,0,0.05)] dark:bg-[rgba(255,255,255,0.05)]">
-        {children}
-      </div>
-    </div>
+    </>
   );
 }
