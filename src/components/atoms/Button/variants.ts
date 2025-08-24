e6a29591cf0c8c11fdbe61af0974df96c3c812cb
atsumarukun <atys.smz@gmail.com>
2025-08-24 23:52:05 +0900

    create: Buttonコンポーネントを作成.

---
+++ src/components/atoms/Button/variants.ts
@@ -0,0 +1,17 @@
+import { cn } from "@/lib/utils";
+
+const base = cn(
+  "flex flex-row items-center gap-2 py-2 px-3",
+  "text-sm rounded-full hover:cursor-pointer focus:outline-none"
+);
+
+export const buttonVariants = {
+  default: cn(base, "bg-primary text-primary-foreground hover:bg-primary/90"),
+  outline: cn(
+    base,
+    "border bg-inherit hover:bg-accent hover:text-accent-foreground"
+  ),
+  ghost: cn(base, "bg-inherit hover:bg-accent hover:text-accent-foreground"),
+} as const;
+
+export type ButtonVariant = keyof typeof buttonVariants;
