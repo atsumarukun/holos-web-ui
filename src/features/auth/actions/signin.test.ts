b8abb6dde4e70dcd1000d468bcca87383db6108a
atsumarukun <atys.smz@gmail.com>
2025-08-30 23:53:51 +0900

    create: signinアクションのテストコードを作成.

---
+++ src/features/auth/actions/signin.test.ts
@@ -0,0 +1,92 @@
+import { signin } from "./signin";
+
+describe("signin", () => {
+  beforeEach(() => {
+    jest.resetModules();
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("success: logged in", async () => {
+    const mockResponse = { token: "1Ty1HKTPKTt8xEi-_3HTbWf2SCHOdqOS" };
+
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      status: 200,
+      json: async () => mockResponse,
+    });
+
+    const result = await signin({
+      accountName: "holos",
+      password: "password",
+    });
+
+    expect(global.fetch).toHaveBeenCalledWith(
+      `${process.env.NEXT_PUBLIC_ACCOUNT_API_HOST}/login`,
+      expect.objectContaining({
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    );
+    expect(result).toEqual({
+      success: true,
+      data: mockResponse,
+    });
+  });
+
+  it("failed: unauthorized", async () => {
+    const mockResponse = { message: "unauthorized" };
+
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: false,
+      status: 401,
+      json: async () => mockResponse,
+    });
+
+    const result = await signin({
+      accountName: "holos",
+      password: "password",
+    });
+
+    expect(result).toEqual({
+      success: false,
+      error: "アカウントが存在しないかパスワードが異なります.",
+    });
+  });
+
+  it("failed: internal server error", async () => {
+    const mockResponse = { message: "internal server error" };
+
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: false,
+      status: 500,
+      json: async () => mockResponse,
+    });
+
+    const result = await signin({
+      accountName: "holos",
+      password: "password",
+    });
+
+    expect(result).toEqual({
+      success: false,
+    });
+  });
+
+  it("failed: occured fetch error", async () => {
+    global.fetch = jest.fn().mockRejectedValue(new Error("error"));
+
+    const result = await signin({
+      accountName: "holos",
+      password: "password",
+    });
+
+    expect(result).toEqual({
+      success: false,
+    });
+  });
+});
