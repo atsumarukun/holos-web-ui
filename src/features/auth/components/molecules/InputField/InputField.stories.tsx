619c29dcdb7f7bce7a2188ba3aa04180e3d5b084
atsumarukun <atys.smz@gmail.com>
2025-09-01 00:01:16 +0900

    create: InputFieldコンポーネントのUIカタログを作成.

---
+++ src/features/auth/components/molecules/InputField/InputField.stories.tsx
@@ -0,0 +1,40 @@
+import { Meta, StoryObj } from "@storybook/nextjs";
+import { InputField } from ".";
+import { LuUserRound } from "react-icons/lu";
+
+const meta = {
+  title: "Molecules/AuthInputField",
+  component: InputField,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    id: {
+      control: false,
+    },
+    icon: {
+      control: false,
+    },
+  },
+} satisfies Meta<typeof InputField>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    placeholder: "アカウント名",
+    id: "name",
+    icon: LuUserRound,
+  },
+};
+
+export const WithError: Story = {
+  args: {
+    placeholder: "アカウント名",
+    error: "アカウント名は必須です.",
+    id: "name",
+    icon: LuUserRound,
+  },
+};
