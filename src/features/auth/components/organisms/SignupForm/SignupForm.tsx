e7035a0be0c9c66418f20157b22914fbe226f478
atsumarukun <atys.smz@gmail.com>
2025-09-01 23:36:04 +0900

    create: SignupFormコンポーネントを作成.

---
+++ src/features/auth/components/organisms/SignupForm/SignupForm.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { LuLockKeyhole, LuUserRound } from "react-icons/lu";
+import { Button } from "@/components/atoms/Button";
+import Link from "next/link";
+import { InputField } from "../../molecules/InputField";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Signup, signupFormSchema } from "./schema";
+import { useState } from "react";
+import { signup } from "@/features/auth/actions/signup";
+import { useRouter } from "next/navigation";
+import { errorToast, successToast } from "@/lib/toast";
+
+export const SignupForm = () => {
+  const router = useRouter();
+
+  const [nameError, setNameError] = useState<string>();
+  const [passwordError, setPasswordError] = useState<string>();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signupFormSchema),
+  });
+
+  const onSubmit: SubmitHandler<Signup> = async (data) => {
+    if (data.password !== data.confirmPassword) {
+      setPasswordError("パスワードが一致しません.");
+      return;
+    }
+
+    const res = await signup(data);
+    if (res.success) {
+      successToast("アカウントを作成しました.");
+      router.push("/auth/signin");
+    } else {
+      if (res.error) {
+        setNameError(res.error);
+      } else {
+        errorToast();
+      }
+    }
+  };
+
+  return (
+    <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col gap-3">
+        <InputField
+          id="name"
+          placeholder="アカウント名"
+          icon={LuUserRound}
+          error={nameError ?? errors.name?.message}
+          registerReturn={register("name")}
+        />
+        <InputField
+          id="password"
+          placeholder="パスワード"
+          type="password"
+          icon={LuLockKeyhole}
+          error={passwordError ?? errors.password?.message}
+          registerReturn={register("password", {
+            onChange: () => setPasswordError(undefined),
+          })}
+        />
+        <InputField
+          id="confirmPassword"
+          placeholder="パスワード(確認)"
+          type="password"
+          icon={LuLockKeyhole}
+          error={passwordError ?? errors.confirmPassword?.message}
+          registerReturn={register("confirmPassword", {
+            onChange: () => setPasswordError(undefined),
+          })}
+        />
+      </div>
+      <div className="flex flex-row items-center justify-between">
+        <Link
+          href="/auth/signin"
+          className="text-xs rounded-full hover:bg-accent hover:text-accent-foreground py-2 px-3 -ml-3"
+        >
+          ログイン
+        </Link>
+        <Button label="作成" />
+      </div>
+    </form>
+  );
+};
---
+++ src/features/auth/components/organisms/SignupForm/schema.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+
+export const signupFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, "3文字以上にしてください.")
+    .max(24, "24文字以下にしてください.")
+    .regex(
+      /^[A-Za-z0-9_]*$/,
+      "大文字, 小文字, 数字, アンダースコアのみ利用できます."
+    ),
+  password: z
+    .string()
+    .min(8, "8文字以上にしてください.")
+    .max(72, "72文字以下にしてください.")
+    .regex(
+      /^[A-Za-z0-9!@#$%^&*()_\-+=\[\]{};:'",.<>?/\\|~]*$]*$/,
+      "大文字, 小文字, 数字, 記号のみ利用できます."
+    ),
+  confirmPassword: z
+    .string()
+    .min(8, "8文字以上にしてください.")
+    .max(72, "72文字以下にしてください.")
+    .regex(
+      /^[A-Za-z0-9!@#$%^&*()_\-+=\[\]{};:'",.<>?/\\|~]*$]*$/,
+      "大文字, 小文字, 数字, 記号のみ利用できます."
+    ),
+});
+
+export type Signup = z.infer<typeof signupFormSchema>;
