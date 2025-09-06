e4d6138a7a34bb40138483fde8990518b7a25c26
atsumarukun <atys.smz@gmail.com>
2025-09-06 18:51:39 +0900

    create: SignupTemplateコンポーネントを作成.

---
+++ src/features/auth/components/templates/SignupTemplate/SignupTemplate.tsx
@@ -0,0 +1,14 @@
+import { Logo } from "@/components/atoms/Logo";
+import { SignupForm } from "../../organisms/SignupForm";
+
+export const SignupTemplate = () => {
+  return (
+    <div className="flex flex-col items-center gap-12">
+      <div className="flex flex-col items-center">
+        <Logo noIcon />
+        <h1 className="text-lg">アカウント作成</h1>
+      </div>
+      <SignupForm />
+    </div>
+  );
+};
