23b2e4146d74a8d1cc19eb3ae3ef540adaa790cc
atsumarukun <atys.smz@gmail.com>
2025-09-07 21:27:00 +0900

    update: アカウント作成ページにメタデータを設定.

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,4 +1,11 @@
 import { SignupTemplate } from "@/features/auth/components/templates/SignupTemplate";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "アカウント作成 - Holos",
+  description:
+    "Holosのアカウント作成ページ。アカウントを作成してサービスの管理が行えます。",
+};
 
 export default function SignupPage() {
   return <SignupTemplate />;
