9fc642595cd96a76b82a60548d70b166b101d5f9
atsumarukun <atys.smz@gmail.com>
2025-09-07 19:46:09 +0900

    create: SigninTemplateコンポーネントのテストコードを作成.

---
+++ src/features/auth/components/templates/SigninTemplate/SigninTemplate.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { SigninTemplate } from "./SigninTemplate";
+
+const pushMock = jest.fn();
+jest.mock("next/navigation", () => ({
+  useRouter: () => ({ push: pushMock }),
+}));
+
+describe("SigninTemplate", () => {
+  it("renders", () => {
+    const { container } = render(<SigninTemplate />);
+    expect(screen.getByRole("paragraph")).toHaveTextContent("Holos");
+    expect(screen.getByRole("heading")).toHaveTextContent("ログイン");
+    expect(container.querySelector("form")).toBeInTheDocument();
+  });
+});
