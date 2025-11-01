4b2572f4fcb59dfd77b71a4f2f839dd192ce036d
atsumarukun <atys.smz@gmail.com>
2025-11-01 18:54:20 +0900

    create: HeaderコンポーネントのUIカタログを作成.

---
+++ src/components/organisms/Header/Header.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from "@storybook/nextjs";
+import { Header } from "./Header";
+import { accountContext } from "@/providers/account";
+
+const meta = {
+  title: "Organisms/Header",
+  component: Header,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
+    <accountContext.Provider value={{ accountName: "sample" }}>
+      <Header />
+    </accountContext.Provider>
+  ),
+};
