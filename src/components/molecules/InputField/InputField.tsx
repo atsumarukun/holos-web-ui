9bac7a481fe752138f101ef8eb07dea1854aa8db
atsumarukun <atys.smz@gmail.com>
2025-12-23 23:44:04 +0900

    create: InputFieldコンポーネントを作成.

---
+++ src/components/molecules/InputField/InputField.tsx
@@ -0,0 +1,39 @@
+import { Input } from "@/components/atoms/Input";
+import { RequiredBadge } from "@/components/atoms/RequiredBadge";
+import { HTMLInputTypeAttribute } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+type Props = Readonly<{
+  id: string;
+  label: string;
+  isRequired?: boolean;
+  error?: string;
+  registerReturn: UseFormRegisterReturn;
+}>;
+
+export const InputField = ({
+  id,
+  label,
+  isRequired,
+  error,
+  registerReturn,
+  ...inputProps
+}: Props) => {
+  return (
+    <div className="flex flex-col gap-0.5">
+      <div className="flex flex-row items-center gap-2">
+        <label htmlFor={id}>{label}</label>
+        {isRequired && <RequiredBadge />}
+      </div>
+      <div className={error ? undefined : "mb-4"}>
+        <Input
+          id={id}
+          variant={error ? "destructive" : "default"}
+          {...inputProps}
+          {...registerReturn}
+        />
+        {error && <p className="text-xs text-destructive">{error}</p>}
+      </div>
+    </div>
+  );
+};
