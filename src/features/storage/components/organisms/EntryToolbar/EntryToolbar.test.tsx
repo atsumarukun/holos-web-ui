2a8b437d26452c26c621247dc1c75719722618db
atsumarukun <atys.smz@gmail.com>
2026-06-03 23:38:43 +0900

    create: EntryToolbarコンポーネントのテストコードを作成.

--- src/features/storage/components/organisms/EntryToolbar/EntryToolbar.test.tsx
+++ src/features/storage/components/organisms/EntryToolbar/EntryToolbar.test.tsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import { VolumeToolbar } from "./VolumeToolbar";
-import userEvent from "@testing-library/user-event";
 import { ReactNode } from "react";
 import { refetchContext } from "@/providers/refetch";
+import { EntryToolbar } from "./EntryToolbar";
+import userEvent from "@testing-library/user-event";
 
 const pathnameMock = jest.fn();
 const searchParamsMock = jest.fn();
@@ -15,7 +15,7 @@ jest.mock("next/navigation", () => ({
 
 const refetchMock = jest.fn();
 
-describe("Storage/Organisms/VolumeToolbar", () => {
+describe("Storage/Organisms/EntryToolbar", () => {
   const renderWithContext = (component: ReactNode) => {
     render(
       <refetchContext.Provider
@@ -27,13 +27,13 @@ describe("Storage/Organisms/VolumeToolbar", () => {
   };
 
   it("renders", () => {
-    renderWithContext(<VolumeToolbar />);
+    renderWithContext(<EntryToolbar volumeName="volume" currentKey="key" />);
     expect(screen.getByPlaceholderText("検索")).toBeInTheDocument();
     expect(screen.getByRole("button", { name: "作成" })).toBeInTheDocument();
   });
 
   it("renders the dialog when opened", async () => {
-    renderWithContext(<VolumeToolbar />);
+    renderWithContext(<EntryToolbar volumeName="volume" currentKey="key" />);
 
     await userEvent.click(screen.getByRole("button", { name: "作成" }));
 
@@ -41,7 +41,7 @@ describe("Storage/Organisms/VolumeToolbar", () => {
   });
 
   it("does not render the dialog when closed", () => {
-    renderWithContext(<VolumeToolbar />);
+    renderWithContext(<EntryToolbar volumeName="volume" currentKey="key" />);
     expect(screen.queryByRole("dialog")).not.toBeInTheDocument();
   });
 });
