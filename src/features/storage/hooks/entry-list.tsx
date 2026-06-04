7ddcca0018c89b2a8ec51093276e53ee76cca04e
atsumarukun <atys.smz@gmail.com>
2026-06-05 00:00:35 +0900

    create: useEntryListフックを作成.

---
+++ src/features/storage/hooks/entry-list.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useCallback, useEffect, useState } from "react";
+import { getEntries, GetEntriesResponse } from "../actions/get-entries";
+import { ActionError } from "@/lib/errors";
+
+type Props = Readonly<{
+  volumeName: string;
+  currentKey: string;
+}>;
+
+export const useEntryList = ({ volumeName, currentKey }: Props) => {
+  const searchParams = useSearchParams();
+
+  const [loading, setLoading] = useState<boolean>(true);
+  const [entries, setEntries] = useState<GetEntriesResponse["entries"]>([]);
+  const [error, setError] = useState<ActionError>();
+
+  const fetch = useCallback(
+    async (
+      props?: Readonly<{
+        onCompleted?: (args?: GetEntriesResponse["entries"]) => void;
+        onError?: (args?: ActionError) => void;
+      }>,
+    ) => {
+      setLoading(true);
+
+      const { data, error } = await getEntries(volumeName, {
+        prefix: currentKey,
+        depth: 1,
+      });
+      if (data) {
+        const searchedEntries = data.entries.filter((entry) =>
+          entry.key.startsWith(searchParams.get("search") ?? ""),
+        );
+        setEntries(searchedEntries);
+        props?.onCompleted?.(searchedEntries);
+      } else {
+        setError(error);
+        props?.onError?.(error);
+      }
+
+      setLoading(false);
+    },
+    [volumeName, currentKey, searchParams],
+  );
+
+  useEffect(() => {
+    fetch();
+  }, [fetch]);
+
+  return {
+    loading,
+    entries,
+    error,
+    refetch: fetch,
+  };
+};
